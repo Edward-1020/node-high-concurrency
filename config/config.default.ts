a38519f3aa3f5cd7142d1952b6b6c4f97205c3a5
yangxu <yangxu@baijiahulian.com>
2020-08-02 17:50:45 +0800

    feat: 引入redis,引入mysql

--- config/config.default.ts
+++ config/config.default.ts
@@ -1,23 +1,28 @@
-import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+import { EggAppConfig, PowerPartial } from 'egg';
 
-export default (appInfo: EggAppInfo) => {
-  const config = {} as PowerPartial<EggAppConfig>;
+export default () => {
+    const config = {} as PowerPartial<EggAppConfig>;
 
-  // override config from framework / plugin
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1596350970437_632';
+    config.sequelize = {
+        dialect: 'mysql',
+        host: '127.0.0.1',
+        port: 3306,
+        username: 'root',
+        password: 'root',
+        database: 'node-high-concurrency'
+    }
 
-  // add your egg config in here
-  config.middleware = [];
+    config.redis = {
+        client: {
+            port: 6379,
+            host: '127.0.0.1',
+            username: '',
+            password: '',
+            db: 0
+        }
+    }
 
-  // add your special config in here
-  const bizConfig = {
-    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
-  };
-
-  // the return config will combines to EggAppConfig
-  return {
-    ...config,
-    ...bizConfig,
-  };
+    return {
+        ...config
+    };
 };
