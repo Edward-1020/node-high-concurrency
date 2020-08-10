ba6edfea61926301b97ebf69f39b1611d58be300
Edward-1020 <iezzuyoung@gmail.com>
2020-08-10 08:11:48 +0800

    feat: md5

--- app/common/utils/md5.ts
+++ app/common/utils/md5.ts
@@ -1,14 +1,14 @@
 import * as crypto from 'crypto';
 
-function md5(src: string) {
+function md5(src: string): string {
     const md5 = crypto.createHash('md5');
     return md5.update(src).digest('hex');
 }
 
 const salt = '1a2b3c4d';
 
-// 表单提交的pass md5,这里的salt写死是因为服务端不知道串是啥
-function inputPassToFormPass(inputPass: string) {
+// 表单提交的明文pass md5,这里的salt写死是因为服务端不知道串是啥
+function inputPassToFormPass(inputPass: string): string {
     const str = salt.charAt(0) + salt.charAt(2) + inputPass + salt.charAt(5) + salt.charAt(4);
     return md5(str);
 }
@@ -16,12 +16,17 @@ function inputPassToFormPass(inputPass: string) {
 // db存的pass md5，salt需要随机
 
 // 表单提交的pass md5,这里的salt写死是因为服务端不知道串是啥
-function formPassToDBPass(formPass: string, salt: string) {
+function formPassToDBPass(formPass: string, salt: string): string {
     const str = salt.charAt(0) + salt.charAt(2) + formPass + salt.charAt(5) + salt.charAt(4);
     return md5(str);
 }
 
+function inputPassToDBPass(inputPass: string, saltDB: string): string {
+    const formPass = inputPassToFormPass(inputPass);
+    return formPassToDBPass(formPass, saltDB);
+}
+
 export {
-    inputPassToFormPass,
-    formPassToDBPass
+    md5,
+    inputPassToDBPass
 }
